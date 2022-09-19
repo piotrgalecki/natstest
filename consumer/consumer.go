3a6eb1d0ad5870b1e80d6ea5d7ff35b70e2ef6a2
Piotr Galecki <piotr.galecki@siden.io>
2022-09-18 23:27:00 -0400

    Add jetstream testing

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -18,8 +18,8 @@ func logEvent(hub int, err error, msg string) {
 	}
 }
 
-func consumer(hub int, natsServer string) {
-	hubMbox := "hub-" + strconv.Itoa(hub)
+func consumer(hub int, natsServer string, useJetstream bool) {
+	hubMbox := "testmbox-" + strconv.Itoa(hub)
 
 	// connect to NATS server
 	var nc *nats.Conn
@@ -35,12 +35,33 @@ func consumer(hub int, natsServer string) {
 	}
 	defer nc.Close()
 
-	// create async subscription to hub-n topic
+	var js nats.JetStreamContext
+	if useJetstream {
+		// Create JetStream Context
+		js, err = nc.JetStream(nats.PublishAsyncMaxPending(256))
+		logEvent(hub, err, "create jetstream context")
+
+		// add jetstream consumer
+		_, err = js.AddConsumer("hubstream",
+			&nats.ConsumerConfig{
+				Durable:       hubMbox,
+				Description:   hubMbox + " consumer",
+				DeliverPolicy: nats.DeliverNewPolicy, // only deliver new messages
+				AckPolicy:     nats.AckExplicitPolicy,
+			})
+		logEvent(hub, err, "create jetstream consumer")
+	}
+
+	// create sync subscription to the hub-n topic
+	var sub *nats.Subscription
 	for {
-		_, err := nc.Subscribe(hubMbox, func(m *nats.Msg) {
-			log.Printf("Hub %d received a message", hub)
-		})
-		logEvent(hub, err, "subscribe to mailbox")
+		subject := "hubmsg." + hubMbox + ".>"
+		if useJetstream {
+			sub, err = js.PullSubscribe(subject, hubMbox)
+		} else {
+			sub, err = nc.SubscribeSync(subject)
+		}
+		logEvent(hub, err, "create subscription to "+subject)
 		if err != nil {
 			time.Sleep(time.Second)
 			continue
@@ -48,14 +69,28 @@ func consumer(hub int, natsServer string) {
 		break
 	}
 
-	var forever chan struct{}
-	<-forever
+	// receive messages
+	for {
+		if useJetstream {
+			msgs, _ := sub.Fetch(10)
+			for _, m := range msgs {
+				m.Ack() // for js use explicit ack
+				log.Println("[Hub ", hub, "] Received js message w/ subject", m.Subject, "data", string(m.Data))
+			}
+		} else {
+			m, err := sub.NextMsg(1 * time.Minute)
+			if err != nil {
+				continue
+			}
+			log.Println("[Hub ", hub, "] Received message w/ subject", m.Subject, "data", string(m.Data))
+		}
+	}
 }
 
 func main() {
 	// get hubCount from environement
 	hubCount := 100
-	hubCountStr := os.Getenv("hubCount")
+	hubCountStr := os.Getenv("HUB_COUNT")
 	if hubCountStr != "" {
 		hubCount64, _ := strconv.ParseInt(hubCountStr, 0, 64)
 		hubCount = int(hubCount64)
@@ -63,9 +98,15 @@ func main() {
 
 	// get natsServer from environement
 	natsServer := "localhost"
-	natsServerStr := os.Getenv("natsServer")
+	natsServerStr := os.Getenv("NATS_SERVER")
 	if natsServerStr != "" {
 		natsServer = natsServerStr
+	}
+
+	useJetstream := false
+	jetStreamStr := os.Getenv("JETSTREAM")
+	if jetStreamStr == "TRUE" || jetStreamStr == "true" {
+		useJetstream = true
 	}
 
 	// get replica number from environement
@@ -77,10 +118,60 @@ func main() {
 		replica = int(replica64)
 	}
 
-	log.Println("NATS consumer test - hubCount", hubCount, "natsServer", natsServer, "replica", replica)
+	log.Println("NATS consumer test - hubCount", hubCount, "natsServer", natsServer, "js", useJetstream, "replica", replica)
 
+	if useJetstream {
+		// connect to NATS server
+		var nc *nats.Conn
+		var err error
+		for {
+			nc, err = nats.Connect(natsServer)
+			logEvent(0, err, "connect to nats server")
+			if err != nil {
+				time.Sleep(time.Second)
+				continue
+			}
+			break
+		}
+		defer nc.Close()
+
+		// Create JetStream Context
+		js, err := nc.JetStream(nats.PublishAsyncMaxPending(256))
+		logEvent(0, err, "create jetstream context")
+
+		// Create JestStream for messages to hubs
+		_, err = js.AddStream(&nats.StreamConfig{
+			Name:              "hubstream",          // stream name
+			Description:       "Hub JetStream",      // description
+			Subjects:          []string{"hubmsg.>"}, // stream subjects
+			Retention:         nats.LimitsPolicy,    // messages are reatined until limit is reached
+			MaxConsumers:      1,                    // max_consumers
+			MaxMsgs:           8000,                 // max_msgs
+			MaxBytes:          0,                    // max_bytes
+			Discard:           nats.DiscardOld,      // discard policy
+			MaxAge:            24 * time.Hour,       // max_age
+			MaxMsgsPerSubject: 0,                    // max_msgs_per_subject
+			MaxMsgSize:        0,                    // max_msg_size
+			Storage:           nats.MemoryStorage,   // storage type
+			Replicas:          2,                    // num_replicas
+			//NoAck             bool            `json:"no_ack,omitempty"`
+			//Template          string          `json:"template_owner,omitempty"`
+			//Duplicates        time.Duration   `json:"duplicate_window,omitempty"`
+			//Placement         *Placement      `json:"placement,omitempty"`
+			//Mirror            *StreamSource   `json:"mirror,omitempty"`
+			//Sources           []*StreamSource `json:"sources,omitempty"`
+			//Sealed            bool            `json:"sealed,omitempty"`
+			//DenyDelete        bool            `json:"deny_delete,omitempty"`
+			//DenyPurge         bool            `json:"deny_purge,omitempty"`
+			//AllowRollup       bool            `json:"allow_rollup_hdrs,omitempty"`
+			//RePublish *SubjectMapping `json:"republish,omitempty // Allow republish of the message after being sequenced and stored
+		})
+		logEvent(0, err, "create jetstream for hub messages")
+	}
+
+	// start hub simulators
 	for hub := replica*hubCount + 1; hub <= replica*hubCount+hubCount; hub++ {
-		go consumer(hub, natsServer)
+		go consumer(hub, natsServer, useJetstream)
 		time.Sleep(10 * time.Millisecond)
 	}
 
--- producer/producer.go
+++ producer/producer.go
@@ -21,7 +21,7 @@ func logEvent(hub int, err error, msg string) {
 func main() {
 	// get hubCount from environement
 	hubCount := 100
-	hubCountStr := os.Getenv("hubCount")
+	hubCountStr := os.Getenv("HUB_COUNT")
 	if hubCountStr != "" {
 		hubCount64, _ := strconv.ParseInt(hubCountStr, 0, 64)
 		hubCount = int(hubCount64)
@@ -29,14 +29,20 @@ func main() {
 
 	// get natsServer from environement
 	natsServer := "localhost"
-	natsServerStr := os.Getenv("natsServer")
+	natsServerStr := os.Getenv("NATS_SERVER")
 	if natsServerStr != "" {
 		natsServer = natsServerStr
 	}
 
+	useJetstream := false
+	jetStreamStr := os.Getenv("JETSTREAM")
+	if jetStreamStr == "TRUE" || jetStreamStr == "true" {
+		useJetstream = true
+	}
+
 	// get msgRate from environement
 	msgRate := 1000
-	msgRateStr := os.Getenv("msgRate")
+	msgRateStr := os.Getenv("MSG_RATE")
 	if msgRateStr != "" {
 		msgRate64, _ := strconv.ParseInt(msgRateStr, 0, 64)
 		msgRate = int(msgRate64)
@@ -44,7 +50,7 @@ func main() {
 
 	// get msgBurst from environement
 	msgBurst := 100
-	msgBurstStr := os.Getenv("msgBurst")
+	msgBurstStr := os.Getenv("MSG_BURST")
 	if msgBurstStr != "" {
 		msgBurst64, _ := strconv.ParseInt(msgBurstStr, 0, 64)
 		msgBurst = int(msgBurst64)
@@ -52,7 +58,8 @@ func main() {
 
 	sleepMs := 1000 / (msgRate / msgBurst)
 
-	log.Println("NATS producer test - hubCount", hubCount, "natsServer", natsServer, "msgRate", msgRate, "msgBurst", msgBurst, "sleepMs", sleepMs)
+	log.Println("NATS producer test - hubCount", hubCount, "natsServer", natsServer, "js", useJetstream,
+		"msgRate", msgRate, "msgBurst", msgBurst, "sleepMs", sleepMs)
 
 	// connect to NATS server
 	var nc *nats.Conn
@@ -68,18 +75,29 @@ func main() {
 	}
 	defer nc.Close()
 
+	var js nats.JetStreamContext
+	if useJetstream {
+		// Create JetStream Context
+		js, err = nc.JetStream(nats.PublishAsyncMaxPending(256))
+		logEvent(0, err, "create jetstream context")
+	}
+
 	hub := 1
 	for {
 		// sent msg burst
 		for i := 0; i < msgBurst; i++ {
-			mboxName := "hub-" + strconv.Itoa(hub)
-			err := nc.Publish(mboxName, []byte("All is Well"))
+			mbox := "testmbox-" + strconv.Itoa(hub)
+			if useJetstream {
+				_, err = js.Publish("hubmsg."+mbox+".JsPerfTest", []byte("All is Well"))
+			} else {
+				err = nc.Publish(mbox+".PerfTest", []byte("All is Well"))
+			}
 			if err != nil {
-				fmt.Println("failed to publish to ", mboxName, err)
+				logEvent(hub, err, "publish message")
 			}
 
 			hub++
-			if hub >= hubCount {
+			if hub > hubCount {
 				fmt.Println("Sent messages to all ", hubCount, " hubs")
 				hub = 1
 			}
